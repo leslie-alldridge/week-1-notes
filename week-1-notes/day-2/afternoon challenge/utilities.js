a819d35fa7ff20855cfd7261bec6820a449ea04e
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-08-11 22:55:44 +1200

    updated notes
    updated notes

--- week-1-notes/day-2/afternoon challenge/utilities.js
+++ week-1-notes/day-2/afternoon challenge/utilities.js
@@ -1,149 +1,73 @@
-// function getType (thing) {
-//     return typeof thing;
-// }
-
 getType = (thing) => typeof thing;
 
-// function isNumber (thing) {
-//     console.log(typeof thing);
-//     if (typeof thing === 'number'){
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
 isNumber = (thing) => typeof thing === 'number' ? true : false;
-
-// function toNumber (str) {
-//     return parseInt(str);
-// }
 
 toNumber = (str) => parseInt(str);
 
-// function isStringNumber (str) {
-//     if (parseInt(str)) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-
 isStringNumber = (str) => parseInt(str) ? true : false;
-
-// function add (a, b) {
-//     return a + b;
-// }
 
 add = (a, b) => a + b
 
-// function addStrings (a, b) {
-//     console.log(a, b)
-//     let n = parseInt(b)
-//     let total = a + n
-//     return total.toString()
-// }
+addStrings = (a, b) => String(add(parseInt(b), a)); //uses the add function above
 
-addStrings = (a, b) => String(add(parseInt(b), a)); //uses the add function above^
-
-// function addStringsOrNumbers (a, b) {
-//     // assigning types to variables and console logging to test
-//     let aType = typeof(a)
-//     console.log(aType);
-//     let bType = typeof(b)
-//     console.log(bType);
-
-//     // if we have two numbers , add them
-//     if (aType && bType === 'number'){
-//         return a + b;
-//     } 
-//     //otherwise convert both to numbers and add, convert output to string
-//     else {
-//         let num = parseInt(a)
-//         num += parseInt(b)
-//         return num.toString();
-//     }
-// }
-
-addStringsOrNumbers = (a, b) => typeof(a) && typeof(b) === 'number' ? a + b : String(parseInt(a) + parseInt(b));
-
-// function isEmail (str) {
-//     let arr = (str.split("@"));
-//     console.log(arr)
-//     if (arr.length < 2){
-//         return false;
-//     } else if (arr[1] === ''){
-//         return false;
-//     } else {
-//         return true;
-//     }
-// }
-
-isEmail = (str) => String(str).match(/^\s*[\w\-\+_]+(?:\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(?:\.[\w\-\+_]+)*\s*$/) ? true : false;
-
-// function countIf (array, fn) {
-//     // we both need to practice this more.
-//     let numCount = 0;
-//     //console.log(fn(array[0]))
-//     for (let i = 0; i < array.length; i++)
-//         if (fn(array[i])){
-//             numCount ++;  
-//     }
-//     return numCount;
-// }
-
-//countIf = (array, fn) => {let count = []; (array.forEach(fn(array), 0))}
-countIf = (array, fn) => (fn(array.forEach())) ? count++ : count = count;
-
-function filter (array, fn) {
-    let count = [];
-    for (let i = 0; i < array.length; i++){
-    if(fn(array[i])){
-        count.push(array[i]);
-    } 
-}
-    return count;
+addStringsOrNumbers = (a, b) => typeof (a) && typeof (b) === 'number' ? a + b :
+    String(parseInt(a) + parseInt(b));
+    
+isEmail = (str) => String(str)
+	.match(
+		/^\s*[\w\-\+_]+(?:\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(?:\.[\w\-\+_]+)*\s*$/
+    ) ? true : false;
+    
+countIf = (array, fn) => {
+	let numCount = 0;
+	for (let i = 0; i < array.length; i++)
+		if (fn(array[i])) {
+			numCount++;
+		}
+	return numCount;
 }
 
-function map (array, fn) {
-    console.log(fn(array[0]))
-    let arrayResult = [];
-    for (let i = 0; i < array.length; i ++){
-        arrayResult.push(fn(array[i]))
-    }
-    return arrayResult;
+filter = (array, fn) => {
+	let count = [];
+	for (let i = 0; i < array.length; i++) {
+		if (fn(array[i])) {
+			count.push(array[i]);
+		}
+	}
+	return count;
 }
 
-function filterStringsWithCommas (str) {
-    let count = 0;
-    //str.filter(",");
-   for (let i = 0; i < str.length; i ++){
-       if(str[i].includes(',')){
-           count ++
-       }
-   }
-   return count;
+map = (array, fn) => {
+	let arrayResult = [];
+	for (let i = 0; i < array.length; i++) {
+		arrayResult.push(fn(array[i]))
+	}
+	return arrayResult;
 }
 
-
-//using split creates a new array!!!
-function splitStringByCommas (str) {
-    return str.split(',')
+filterStringsWithCommas = (str) => {
+	let count = 0;
+	for (let i = 0; i < str.length; i++) {
+		if (str[i].includes(',')) {
+			count++
+		}
+	}
+	return count;
 }
-
-
+splitStringByCommas = (str) => str.split(',')
 
 module.exports = {
-    getType: getType,
-    isNumber: isNumber,
-    toNumber: toNumber,
-    isStringNumber: isStringNumber,
-    add: add,
-    addStrings: addStrings,
-    addStringsOrNumbers: addStringsOrNumbers,
-    isEmail: isEmail,
-    countIf: countIf,
-    filter: filter,
-    map: map,
-    filterStringsWithCommas: filterStringsWithCommas,
-    splitStringByCommas: splitStringByCommas
-  }
+	getType,
+	isNumber,
+	toNumber,
+	isStringNumber,
+	add,
+	addStrings,
+	addStringsOrNumbers,
+	isEmail,
+	countIf,
+	filter,
+	map,
+	filterStringsWithCommas,
+	splitStringByCommas
+}
--- week-1-notes/day-2/morning challenge/types.js
+++ week-1-notes/day-2/morning challenge/types.js
@@ -1,34 +1,16 @@
-function getFunction() {
-    let func = function(){
-    }
-    return func;
+getFunction = () => {
+	let func = () => {}
+	return func;
 }
-
-function getNull() {
-    return null;
+getObject = () => {
+	let obj = {};
+	return obj;
 }
-
-function getNumber() {
-    let num = 10;
-    return num;
-}
-
-function getObject(){
-    let obj = {};
-    return obj;
-}
-
-function getString(){
-    let str = '';
-    return str;
-}
-
 module.exports = {
-    getBoolean: () => true,   //shortcut for exporting a function that evaluates to true
-    getFunction: getFunction,
-    getNull: getNull,
-    getNumber: getNumber,
-    getObject: getObject,
-    getString: getString
+	getBoolean: () => true,
+	getFunction,
+	getNull: () => null,
+	getNumber: () => 10,
+	getObject,
+	getString: () => ''
 };
-
